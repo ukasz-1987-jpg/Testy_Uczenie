d898f136982ab64c74c2259d06bd491519336d31
ukasz-1987-jpg <ukasz-1987@o2.pl>
2026-05-27 12:27:25 +0000

    Kod Dziala

---
+++ import os.py
@@ -0,0 +1,9 @@
+# Lista samochodów
+cars = ["Audi", "BMW", "Toyota", "Ford", "Mercedes"]
+
+# Komunikat
+print("Lista samochodów:\n")
+
+# Wylistowanie zawartości listy
+for car in cars:
+    print(car)
